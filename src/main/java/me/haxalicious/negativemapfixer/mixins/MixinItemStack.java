3932029cc08da7897d991af98df7219f44c156ba
haxalicious <haxalicious@github.com>
2020-10-27 18:32:09 -0500

    Fixed mixins

--- src/main/java/me/haxalicious/negativemapfixer/mixins/MixinItemStack.java
+++ src/main/java/me/haxalicious/negativemapfixer/mixins/MixinItemStack.java
@@ -7,6 +7,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 /**
  * @author Haxalicious
  * @since 10/27/20
@@ -16,13 +18,15 @@ public class MixinItemStack {
     @Shadow
     private int itemDamage;
 
-    @Inject(method = "<init>", at = @At("RETURN"), cancellable = true)
-    public void ItemStackPOST (Item itemIn, int amount, int meta) {
+    @Inject(method = "<init>(Lnet/minecraft/item/Item;II)V", at = @At("RETURN"), cancellable = true)
+    public void ItemStackPOST (Item itemIn, int amount, int meta, CallbackInfo cb) {
         this.itemDamage = meta;
+        cb.cancel();
     }
-    @Inject(method = "<init>", at = @At("RETURN"), cancellable = true)
-    public void ItemStackPOST (NBTTagCompound compound) {
+    @Inject(method = "<init>(Lnet/minecraft/nbt/NBTTagCompound;)V", at = @At("RETURN"), cancellable = true)
+    public void ItemStackPOST (NBTTagCompound compound, CallbackInfo cb) {
         this.itemDamage = compound.getShort("Damage");
+        cb.cancel();
     }
 
 }
